f7dd533446bb21f74753fa66fb0bfced8e3169d4
Nathanial <nathanial28@gmail.com>
2023-06-19 04:55:06 -0600

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
+import { books } from "./books";
+import { Book } from "./Book";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = () => {        
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+        <>
+            <h1 className="title">amazon best sellers</h1>            
+            <section className="booklist">
+            {
+                books.map((book, index)=>{                               
+                    return <Book {...book} key={book.id} index={index+1}/>    
+                })
+            }        
+            </section>
+        </>
+    )    
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BookList/>)
+
+export default BookList;
